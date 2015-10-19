19f5771b57318753019e3ff64e1afae3b212daa5
Dagar <dagar90@gmail.com>
2015-10-19 10:59:55 +0000

    Create static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    def full_title(name)
+        base_title = "Twitter"
+        if name.empty?
+            base_title
+        else
+            base_title + " | #{name}"
+        end
+    end
+    
 end

4ccbd4151f7dc0b96f094bfd71854afac7828a43
Dagar <dagar90@gmail.com>
2015-10-26 11:12:43 +0000

    Chapter 9 part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -19,6 +22,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Профиль успешно обновлен"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
   
    private
 
@@ -26,6 +44,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
+    
+    def signed_in_user
+      unless signed_in?
+        flash[:danger] = "Please sign in."
+        store_location
+        redirect_to signin_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
   
 end
